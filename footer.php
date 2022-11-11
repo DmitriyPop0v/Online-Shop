f4c744f0d45ec9c39be8743e6f6c2797a14a29a2
Anton <86726028+AaronR92@users.noreply.github.com>
2022-11-12 01:14:11 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
 						<div class="col-md-3 col-xs-6">
 							<div class="footer">
 								<h3 class="footer-title">About Us</h3>
-								<p>This is our Small System. Made By Ruzanoff, Popoff, Skorik</p>
+								<p>This is our Small System. Made By Ruzanoff, Popoff, Skorikoff</p>
 								<ul class="footer-links">
 									<li><a href="#"><i class="fa fa-map-marker"></i>Krasnodar, India</a></li>
 									<li><a href="#"><i class="fa fa-phone"></i>+7(918)1457821</a></li>
